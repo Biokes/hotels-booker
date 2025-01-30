21bcb8d8668076bb41b3fdda3942e7b6683291c2
Biokes <106444765+Biokes@users.noreply.github.com>
2025-01-30 11:32:45 +0100

    add

--- components/contact/contacts.tsx
+++ components/contact/contacts.tsx
@@ -3,7 +3,6 @@ import Navbar from "@/components/reuseables/navbar";
 import AboutHeader from "@/components/about_us/header";
 import Message from "@/components/contact/message";
 
-
 export default function ContactPage(){
     return (
         <>
