f899b4bc40271d75aa2cd5d1b762684d15a4cc3b
Biokes <106444765+Biokes@users.noreply.github.com>
2025-01-30 11:29:27 +0100

    fix: reolve esling issue

--- components/contact/contacts.tsx
+++ components/contact/contacts.tsx
@@ -1,10 +1,8 @@
 'use client'
 import Navbar from "@/components/reuseables/navbar";
 import AboutHeader from "@/components/about_us/header";
-import Footer from "@/components/reuseables/footer";
 import Message from "@/components/contact/message";
-import ReduxProvider from "@/app/ReduxProvider";
-import BookingModal from "@/components/home/bookModal";
+
 
 export default function ContactPage(){
     return (
